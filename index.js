e5a1dcecade4a036f7145aca8b37bcc823c35a46
Ezekiel Ishaya <ezekielsiniishaya@gmail.com>
2024-09-27 11:28:16 +0100

    updating files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const fs = require("fs");
 
